41c61d9e4b80c8904fd0fa12f2141a4ba93328d8
David Foster <david@applied-data.science>
2018-12-24 22:17:54 +0000

    latest

--- cyclegan/cyclegan.py
+++ cyclegan/cyclegan.py
@@ -8,24 +8,36 @@ from keras.layers import BatchNormalization, Activation, ZeroPadding2D
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.convolutional import UpSampling2D, Conv2D
 from keras.models import Sequential, Model
+from keras.initializers import RandomNormal
 from keras.optimizers import Adam
+from keras import backend as K
 import datetime
 import matplotlib.pyplot as plt
 import sys
 from data_loader import DataLoader
 import numpy as np
 import os
+import pickle
+
+def half_mse(y_true, y_pred):
+    return 0.5 * K.mean(K.square(y_pred - y_true), axis=-1)
+
+
 
 class CycleGAN():
     def __init__(self):
         # Input shape
-        self.img_rows = 128
-        self.img_cols = 128
+        self.img_rows = 256
+        self.img_cols = 256
         self.channels = 3
         self.img_shape = (self.img_rows, self.img_cols, self.channels)
 
+        self.d_losses = []
+        self.g_losses = []
+        self.epoch = 0
+
         # Configure data loader
-        self.dataset_name = 'apple2orange'
+        self.dataset_name = 'vangogh2photo'
         self.data_loader = DataLoader(dataset_name=self.dataset_name,
                                       img_res=(self.img_rows, self.img_cols))
 
@@ -34,30 +46,30 @@ class CycleGAN():
         patch = int(self.img_rows / 2**4)
         self.disc_patch = (patch, patch, 1)
 
+        self.weight_init = RandomNormal(mean=0., stddev=0.02)
+
         # Number of filters in the first layer of G and D
         self.gf = 32
         self.df = 64
 
         # Loss weights
+        self.validation_weight = 1.0
         self.lambda_cycle = 10.0                    # Cycle-consistency loss
-        self.lambda_id = 0.1 * self.lambda_cycle    # Identity loss
+        self.lambda_id = 5.0    # Identity loss
 
         optimizer = Adam(0.0002, 0.5)
 
         # Build and compile the discriminators
         self.d_A = self.build_discriminator()
         self.d_B = self.build_discriminator()
-        self.d_A.compile(loss='mse',
-            optimizer=optimizer,
+        self.d_A.compile(loss=half_mse,
+            optimizer=Adam(0.0002, 0.5),
             metrics=['accuracy'])
-        self.d_B.compile(loss='mse',
-            optimizer=optimizer,
+        self.d_B.compile(loss=half_mse,
+            optimizer=Adam(0.0002, 0.5),
             metrics=['accuracy'])
 
-        #-------------------------
-        # Construct Computational
-        #   Graph of Generators
-        #-------------------------
+        # Build and compile the generators
 
         # Build the generators
         self.g_AB = self.build_generator()
@@ -93,17 +105,19 @@ class CycleGAN():
         self.combined.compile(loss=['mse', 'mse',
                                     'mae', 'mae',
                                     'mae', 'mae'],
-                            loss_weights=[  1, 1,
+                            loss_weights=[  self.validation_weight, self.validation_weight,
                                             self.lambda_cycle, self.lambda_cycle,
                                             self.lambda_id, self.lambda_id ],
-                            optimizer=optimizer)
+                            optimizer=Adam(0.0002, 0.5))
+
+    
 
     def build_generator(self):
         """U-Net Generator"""
 
         def conv2d(layer_input, filters, f_size=4):
             """Layers used during downsampling"""
-            d = Conv2D(filters, kernel_size=f_size, strides=2, padding='same')(layer_input)
+            d = Conv2D(filters, kernel_size=f_size, strides=2, padding='same',kernel_initializer = self.weight_init)(layer_input)
             d = LeakyReLU(alpha=0.2)(d)
             d = InstanceNormalization()(d)
             return d
@@ -111,7 +125,7 @@ class CycleGAN():
         def deconv2d(layer_input, skip_input, filters, f_size=4, dropout_rate=0):
             """Layers used during upsampling"""
             u = UpSampling2D(size=2)(layer_input)
-            u = Conv2D(filters, kernel_size=f_size, strides=1, padding='same', activation='relu')(u)
+            u = Conv2D(filters, kernel_size=f_size, strides=1, padding='same', activation='relu', kernel_initializer = self.weight_init)(u)
             if dropout_rate:
                 u = Dropout(dropout_rate)(u)
             u = InstanceNormalization()(u)
@@ -133,7 +147,7 @@ class CycleGAN():
         u3 = deconv2d(u2, d1, self.gf)
 
         u4 = UpSampling2D(size=2)(u3)
-        output_img = Conv2D(self.channels, kernel_size=4, strides=1, padding='same', activation='tanh')(u4)
+        output_img = Conv2D(self.channels, kernel_size=4, strides=1, padding='same', activation='tanh',kernel_initializer = self.weight_init)(u4)
 
         return Model(d0, output_img)
 
@@ -141,7 +155,7 @@ class CycleGAN():
 
         def d_layer(layer_input, filters, f_size=4, normalization=True):
             """Discriminator layer"""
-            d = Conv2D(filters, kernel_size=f_size, strides=2, padding='same')(layer_input)
+            d = Conv2D(filters, kernel_size=f_size, strides=2, padding='same',kernel_initializer = self.weight_init)(layer_input)
             d = LeakyReLU(alpha=0.2)(d)
             if normalization:
                 d = InstanceNormalization()(d)
@@ -154,7 +168,7 @@ class CycleGAN():
         d3 = d_layer(d2, self.df*4)
         d4 = d_layer(d3, self.df*8)
 
-        validity = Conv2D(1, kernel_size=4, strides=1, padding='same')(d4)
+        validity = Conv2D(1, kernel_size=4, strides=1, padding='same',kernel_initializer = self.weight_init)(d4)
 
         return Model(img, validity)
 
@@ -166,7 +180,7 @@ class CycleGAN():
         valid = np.ones((batch_size,) + self.disc_patch)
         fake = np.zeros((batch_size,) + self.disc_patch)
 
-        for epoch in range(epochs):
+        for epoch in range(self.epoch, epochs):
             for batch_i, (imgs_A, imgs_B) in enumerate(self.data_loader.load_batch(batch_size)):
 
                 # ----------------------
@@ -187,7 +201,16 @@ class CycleGAN():
                 dB_loss = 0.5 * np.add(dB_loss_real, dB_loss_fake)
 
                 # Total disciminator loss
-                d_loss = 0.5 * np.add(dA_loss, dB_loss)
+                d_loss_total = 0.5 * np.add(dA_loss, dB_loss)
+
+                d_loss = [
+                    d_loss_total[0]
+                    , dA_loss[0], dA_loss_real[0], dA_loss_fake[0]
+                    , dB_loss[0], dB_loss_real[0], dB_loss_fake[0]
+                    , d_loss_total[1]
+                    , dA_loss[1], dA_loss_real[1], dA_loss_fake[1]
+                    , dB_loss[1], dB_loss_real[1], dB_loss_fake[1]
+                ]
 
 
                 # ------------------
@@ -204,29 +227,36 @@ class CycleGAN():
 
                 # Plot the progress
                 print ("[Epoch %d/%d] [Batch %d/%d] [D loss: %f, acc: %3d%%] [G loss: %05f, adv: %05f, recon: %05f, id: %05f] time: %s " \
-                                                                        % ( epoch, epochs,
-                                                                            batch_i, self.data_loader.n_batches,
-                                                                            d_loss[0], 100*d_loss[1],
-                                                                            g_loss[0],
-                                                                            np.mean(g_loss[1:3]),
-                                                                            np.mean(g_loss[3:5]),
-                                                                            np.mean(g_loss[5:6]),
-                                                                            elapsed_time))
+                    % ( self.epoch, epochs,
+                        batch_i, self.data_loader.n_batches,
+                        d_loss_total[0], 100*d_loss_total[1],
+                        g_loss[0],
+                        np.sum(g_loss[1:3]),
+                        np.sum(g_loss[3:5]),
+                        np.sum(g_loss[5:7]),
+                        elapsed_time))
+
+                self.d_losses.append(d_loss)
+                self.g_losses.append(g_loss)
 
                 # If at save interval => save generated image samples
                 if batch_i % sample_interval == 0:
-                    self.sample_images(epoch, batch_i)
+                    self.sample_images(batch_i)
+                    self.save_model()
 
-    def sample_images(self, epoch, batch_i):
+                
+            self.epoch += 1
+
+    def sample_images(self, batch_i):
         os.makedirs('images/%s' % self.dataset_name, exist_ok=True)
-        r, c = 2, 3
+        r, c = 2, 4
 
         imgs_A = self.data_loader.load_data(domain="A", batch_size=1, is_testing=True)
         imgs_B = self.data_loader.load_data(domain="B", batch_size=1, is_testing=True)
 
         # Demo (for GIF)
-        #imgs_A = self.data_loader.load_img('datasets/apple2orange/testA/n07740461_1541.jpg')
-        #imgs_B = self.data_loader.load_img('datasets/apple2orange/testB/n07749192_4241.jpg')
+        # imgs_A = self.data_loader.load_img('datasets/%s/testA/n07740461_14740.jpg' % self.dataset_name)
+        # imgs_B = self.data_loader.load_img('datasets/%s/testB/n07749192_4241.jpg' % self.dataset_name)
 
         # Translate images to the other domain
         fake_B = self.g_AB.predict(imgs_A)
@@ -235,13 +265,18 @@ class CycleGAN():
         reconstr_A = self.g_BA.predict(fake_B)
         reconstr_B = self.g_AB.predict(fake_A)
 
-        gen_imgs = np.concatenate([imgs_A, fake_B, reconstr_A, imgs_B, fake_A, reconstr_B])
+        # ID the images
+        id_A = self.g_BA.predict(imgs_A)
+        id_B = self.g_AB.predict(imgs_B)
+
+        gen_imgs = np.concatenate([imgs_A, fake_B, reconstr_A, id_A, imgs_B, fake_A, reconstr_B, id_B])
 
         # Rescale images 0 - 1
         gen_imgs = 0.5 * gen_imgs + 0.5
+        gen_imgs = np.clip(gen_imgs, 0, 1)
 
-        titles = ['Original', 'Translated', 'Reconstructed']
-        fig, axs = plt.subplots(r, c)
+        titles = ['Original', 'Translated', 'Reconstructed', 'ID']
+        fig, axs = plt.subplots(r, c, figsize=(15,7.5))
         cnt = 0
         for i in range(r):
             for j in range(c):
@@ -249,10 +284,22 @@ class CycleGAN():
                 axs[i, j].set_title(titles[j])
                 axs[i,j].axis('off')
                 cnt += 1
-        fig.savefig("images/%s/%d_%d.png" % (self.dataset_name, epoch, batch_i))
+        fig.savefig("images/%s/%d_%d.png" % (self.dataset_name, self.epoch, batch_i))
         plt.close()
+
+    def save_model(self):
+
+        os.makedirs('saved_model/%s' % self.dataset_name, exist_ok=True)
+
+        self.combined.save_weights('saved_model/%s/model-%d.h5' % (self.dataset_name, self.epoch))
+        self.d_A.save_weights('saved_model/%s/d_A-%d.h5' % (self.dataset_name, self.epoch))
+        self.d_B.save_weights('saved_model/%s/d_B-%d.h5' % (self.dataset_name, self.epoch))
+        self.g_BA.save_weights('saved_model/%s/g_BA-%d.h5' % (self.dataset_name, self.epoch))
+        self.g_AB.save_weights('saved_model/%s/g_AB-%d.h5' % (self.dataset_name, self.epoch))
+                
+        pickle.dump(self, open( "saved_model/%s/obj.pkl" % (self.dataset_name), "wb" ))
 
 
 if __name__ == '__main__':
     gan = CycleGAN()
-    gan.train(epochs=200, batch_size=1, sample_interval=200)
+    gan.train(epochs=200, batch_size=1, sample_interval=100)
