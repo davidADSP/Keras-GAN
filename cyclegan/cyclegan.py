427287fadae4fe8e7317be57a198da51d56928ef
David Foster <david@applied-data.science>
2018-12-27 09:29:06 +0000

    latest

--- cyclegan/cyclegan.py
+++ cyclegan/cyclegan.py
@@ -119,7 +119,7 @@ class CycleGAN():
             """Layers used during downsampling"""
             d = Conv2D(filters, kernel_size=f_size, strides=2, padding='same',kernel_initializer = self.weight_init)(layer_input)
             d = LeakyReLU(alpha=0.2)(d)
-            d = InstanceNormalization()(d)
+            d = InstanceNormalization(axis = -1, center = False, scale = False)(d)
             return d
 
         def deconv2d(layer_input, skip_input, filters, f_size=4, dropout_rate=0):
@@ -128,7 +128,7 @@ class CycleGAN():
             u = Conv2D(filters, kernel_size=f_size, strides=1, padding='same', activation='relu', kernel_initializer = self.weight_init)(u)
             if dropout_rate:
                 u = Dropout(dropout_rate)(u)
-            u = InstanceNormalization()(u)
+            u = InstanceNormalization(axis = -1, center = False, scale = False)(u)
             u = Concatenate()([u, skip_input])
             return u
 
@@ -158,7 +158,7 @@ class CycleGAN():
             d = Conv2D(filters, kernel_size=f_size, strides=2, padding='same',kernel_initializer = self.weight_init)(layer_input)
             d = LeakyReLU(alpha=0.2)(d)
             if normalization:
-                d = InstanceNormalization()(d)
+                d = InstanceNormalization(axis = -1, center = False, scale = False)(d)
             return d
 
         img = Input(shape=self.img_shape)
